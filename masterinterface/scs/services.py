b75781a85cc91c87f398f07130773192f277d254
mbalasso <m.balasso@scsitaly.com>
2012-04-03 14:33:01 +0200

    fix comments

--- masterinterface/scs/services.py
+++ masterinterface/scs/services.py
@@ -51,25 +51,25 @@ def invokeSoapService( wsdl_url, service, port, method, requestParameters, reque
 
     # if response is a list the type in wsdl is arrayofType
     return response
+    
     ##To Continue,if Mokup view need to render complex response, have to change it.
-    """
-    result={}
-    # marshall response
-    if type(response) == 'Text':
-        result=response
-    else:
-        for name, Type in responseType:
-            # arg is a tuple (name, type)
 
-            if Type == 'ComplexType':
-                complexType = client.factory.create( name )
-                for key, value in complexType:
-                    result[key] =  response[key]
+    #    result={}
+    #    # marshall response
+    #    if type(response) == 'Text':
+    #        result=response
+    #    else:
+    #        for name, Type in responseType:
+    #            # arg is a tuple (name, type)
+    #
+    #            if Type == 'ComplexType':
+    #                complexType = client.factory.create( name )
+    #                for key, value in complexType:
+    #                    result[key] =  response[key]
+    #
+    #            result[name] = response[name]
+    #    return result
 
-            result[name] = response[name]
-    return result
-
-    """
 
 
 
