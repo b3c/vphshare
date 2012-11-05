366df24132b2ee00b5f5a4fb7147acb4028eacad
b3c <web.developers@scsitaly.com>
2012-11-05 17:02:40 +0100

    now postgress need TZ format for timestamp

--- masterinterface/settings.py
+++ masterinterface/settings.py
@@ -76,6 +76,7 @@ USE_I18N = True
 # If you set this to False, Django will not format dates, numbers and
 # calendars according to the current locale
 USE_L10N = True
+USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
