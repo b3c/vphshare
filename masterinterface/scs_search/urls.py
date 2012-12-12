57d0897080f8e010ac1e50a21b9f671c77dbc727
“sprosapio” <s.prosapio@scsitaly.com>
2012-12-12 11:44:40 +0100

    Optimization python code following Pylint

--- masterinterface/scs_search/urls.py
+++ masterinterface/scs_search/urls.py
@@ -1,19 +1,33 @@
-__author__ = ""
+"""
+    scs_search: urls.py Module
+"""
+__author__ = ''
 
-from django.conf.urls import patterns, url, include
-from views import *
-#from piston.resource import Resource
+from django.conf.urls import patterns, url
+from views import automatic_search_view, automatic_search_service, \
+    guided_search_s1_service, guided_search_s2_service, complex_query_service
 
-#automatic_search_entries = Resource( handler=AutomaticSearchService)
-#guided_search_s1_entries = Resource( handler=GuidedSearchS1Service)
-#guided_search_s2_entries = Resource( handler=GuidedSearchS2Service)
+
+# EXAMPLE USE WITH DJANGO-PISTON
+# from piston.resource import Resource
+# automatic_search_entries = Resource( handler=AutomaticSearchService)
+
 
 urlpatterns = patterns(
-
     'scs_search.views',
-    url( r'^search/$', automaticSearchView , name='automaticSearchView' ),
-    url( r'^automatic_search/', automaticSearchService, name='AutomaticSearchService' ) ,
-    url( r'^guided_search_s1/', guidedSearchS1Service, name='guidedSearchS1Service' ) ,
-    url( r'^guided_search_s2/', guidedSearchS2Service, name='guidedSearchS2Service' ) ,
-    url( r'^guided_search_complex_query/', guidedSearchComplexQueryService, name='guidedSearchComplexQueryService' ) ,
+
+    url(r'^search/$', automatic_search_view, name='automaticSearchView'),
+
+    url(r'^automatic_search/', automatic_search_service,
+        name='AutomaticSearchService'),
+
+    url(r'^guided_search_s1/', guided_search_s1_service,
+        name='guidedSearchS1Service'),
+
+    url(r'^guided_search_s2/', guided_search_s2_service,
+        name='guidedSearchS2Service'),
+
+    url(r'^guided_search_complex_query/',
+        complex_query_service,
+        name='guidedSearchComplexQueryService'),
     )
--- masterinterface/scs_search/views.py
+++ masterinterface/scs_search/views.py
@@ -1,123 +1,134 @@
-__author__ = ""
+"""
+    scs_search: views.py Module
+"""
+__author__ = ''
+
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.views.decorators.csrf import csrf_exempt
 from django.utils import simplejson
-#from piston.handler import BaseHandler
 from urllib import quote
-from connector import automaticSearchConnector
-from connector import guidedSearchS1Connector
-from connector import guidedSearchS2Connector
-from connector import guidedSearchComplexQueryConnector
+from connector import automatic_search_connector
+from connector import guided_search_s1_connector
+from connector import guided_search_s2_connector
+from connector import complex_query_connector
 
 
-def automaticSearchView( request ):
+def automatic_search_view( request ):
     """
-        automaticSearch view
-    """
-
-    return render_to_response("scs_search/scs_search.html",
-        None,
-        RequestContext(request))
-
-
-def guidedSearchView( request ):
-    """
-        guidedSearch view
+        Automatic Search view
     """
 
-    return render_to_response("scs_search/scs_search.html",
-        None,
-        RequestContext(request))
+    return render_to_response( 'scs_search/scs_search.html', None,
+                              RequestContext( request ) )
+
+
+def guided_search_view( request) :
+    """
+        Guided Search view
+    """
+
+    return render_to_response( 'scs_search/scs_search.html', None,
+                              RequestContext( request ) )
 
 
 @csrf_exempt
-def automaticSearchService( request ):
+def automatic_search_service( request ):
     """
+        Automatic Search Service
     """
-    if request.method == "POST":
 
-        free_text = request.POST['input']
-        connector = automaticSearchConnector( quote( free_text ) )
+    if request.method == 'POST':
 
-        response = HttpResponse(content=connector, content_type="application/json")
+        free_text = request.POST[ 'input' ]
+        connector = automatic_search_connector( quote( free_text ) )
+
+        response = HttpResponse( content = connector,
+                                content_type = 'application/json' )
         return response
 
-    response = HttpResponse(status=403)
+    response = HttpResponse( status = 403 )
     response._is_string = True
 
     return response
 
 
 @csrf_exempt
-def guidedSearchS1Service( request ):
+def guided_search_s1_service(request):
     """
+        Guided Search Step1 Service
     """
-    if request.method == "POST":
 
-        free_text = request.POST['input']
-        nummaxhits = request.POST['nummaxhits']
-        pagenum = request.POST['pagenum']
+    if request.method == 'POST':
 
-        connector = guidedSearchS1Connector( quote( free_text ), nummaxhits,  pagenum )
+        free_text = request.POST[ 'input' ]
+        num_max_hits = request.POST[ 'nummaxhits' ]
+        page_num = request.POST[ 'pagenum' ]
 
-        response = HttpResponse(content=connector, content_type="application/json")
+        connector = guided_search_s1_connector( quote( free_text ),
+                num_max_hits, page_num )
+
+        response = HttpResponse( content = connector,
+                                content_type = 'application/json' )
 
         return response
 
-    response = HttpResponse(status=403)
+    response = HttpResponse( status = 403 )
     response._is_string = True
 
     return response
 
 
 @csrf_exempt
-def guidedSearchS2Service( request ):
+def guided_search_s2_service( request ):
     """
+        Guided Search Step2 Service
     """
-    if request.method == "POST":
 
-        concept_uri_list = request.POST['concept_uri_list']
-        connector = guidedSearchS2Connector( quote( concept_uri_list ) )
+    if request.method == 'POST':
 
-        response = HttpResponse(content=connector, content_type="application/json")
+        concept_uri_list = request.POST[ 'concept_uri_list' ]
+        connector = guided_search_s2_connector( quote( concept_uri_list ) )
+
+        response = HttpResponse( content = connector,
+                                content_type = 'application/json' )
         response._is_string = False
 
         return response
 
-    response = HttpResponse(status=403)
+    response = HttpResponse( status = 403 )
     response._is_string = True
 
     return response
 
 
 @csrf_exempt
-def guidedSearchComplexQueryService( request ):
+def complex_query_service( request ):
     """
+        Complex Query Service
     """
 
-    terms = ""
+    terms = ''
 
-    if request.method == "POST":
+    if request.method == 'POST':
 
-        groups_query = request.POST['groups_query']
+        groups_query = request.POST[ 'groups_query' ]
         load_groups = simplejson.loads( groups_query )
 
-        for j, group in enumerate(load_groups):
-
+        for ( j, group ) in enumerate( load_groups ):
             if j == 0:
-                if len(group) > 1:
-                    for i, concept in enumerate(group):
-                        if i < len(group) - 1:
+                if len( group ) > 1:
+                    for ( i, concept ) in enumerate( group ):
+                        if i < len( group ) - 1:
                             terms = terms + '(' + concept + ' OR '
                         else:
                             terms = terms + concept + ')'
                 else:
                     terms = terms + '(' + group[0] + ')'
             else:
-                if len(group) > 1:
-                    for i, concept in enumerate(group):
+                if len( group ) > 1:
+                    for ( i, concept ) in enumerate( group ):
                         if i < len(group) - 1:
                             terms = terms + ' AND (' + concept + ' OR '
                         else:
@@ -125,14 +136,15 @@ def guidedSearchComplexQueryService( request ):
                 else:
                     terms = terms + ' AND (' + group[0] + ')'
 
-        connector = guidedSearchComplexQueryConnector( quote( terms ) )
+        connector = complex_query_connector( quote( terms ) )
 
-        response = HttpResponse(content=connector, content_type="application/json")
+        response = HttpResponse( content = connector,
+                                content_type = 'application/json ')
         response._is_string = False
 
         return response
 
-    response = HttpResponse(status=403)
+    response = HttpResponse( status = 403 )
     response._is_string = True
 
     return response
