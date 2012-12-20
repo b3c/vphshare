9fcec18b1145ffed5ad8fec44b9dd0b770f2a1b0
Alfredo Saglimbeni <a.saglimbeni@scsitaly.com>
2012-12-20 12:19:32 +0100

    update migration fo scs_search

--- masterinterface/scs_search/views.py
+++ masterinterface/scs_search/views.py
@@ -170,7 +170,7 @@ def save_complex_query( request ):
             query_obj.save()
             query_obj.user.add( user )
             
-            response = HttpResponse()
+            response = HttpResponse(status=200, content_type = 'application/json ')
             response._is_string = False
 
             return response
