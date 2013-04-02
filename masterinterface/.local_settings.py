9aa63f9610e5fd57375801b1a1ee2d0143fe0aa1
Alfredo Saglimbeni <a.saglimbeni@scsitaly.com>
2013-04-02 12:21:39 +0200

    update local settings

--- masterinterface/.local_settings.py
+++ masterinterface/.local_settings.py
@@ -1,12 +1,10 @@
-import os
-from django.conf import settings
 
 DEBUG = True
 
 DATABASES = {
     "default": {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(settings.PROJECT_ROOT, 'vphshare.db'),                      # Or path to database file if using sqlite3.
+        'NAME': 'vphshare.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
