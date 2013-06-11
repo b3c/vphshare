c752527c6c2fd4c0a652a148bd761381d2c52e77
Daniel Harezlak <d.harezlak@cyfronet.pl>
2013-06-11 14:20:18 +0200

    global settings.py fixed

--- masterinterface/settings.py
+++ masterinterface/settings.py
@@ -18,14 +18,14 @@ MANAGERS = ADMINS
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
-DEFAULT_DB = {
-   'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-    'NAME': os.path.join(PROJECT_ROOT, 'vphshare.db'),                      # Or path to database file if using sqlite3.
-    'USER': '',                      # Not used with sqlite3.
-    'PASSWORD': '',                  # Not used with sqlite3.
-    'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-    'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-}
+#DEFAULT_DB = {
+#   'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+#    'NAME': os.path.join(PROJECT_ROOT, 'vphshare.db'),                      # Or path to database file if using sqlite3.
+#    'USER': '',                      # Not used with sqlite3.
+#    'PASSWORD': '',                  # Not used with sqlite3.
+#    'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+#    'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+#}
 
 # Cyfronet Database
 CYFRONET_DB = {
@@ -39,7 +39,14 @@ CYFRONET_DB = {
 
 
 DATABASES = {
-    'default': DEFAULT_DB
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'vphsharedb',                      # Or path to database file if using sqlite3.
+        'USER': 'vph',                      # Not used with sqlite3.
+        'PASSWORD': 'vph.0RG',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
 }
 
 #Define class where extened user profile
