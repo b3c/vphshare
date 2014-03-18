db63fca6674f6bd01b684a0859f069ae1ec9f709
Alfredo Saglimbeni <a.saglimbeni@scsitaly.com>
2014-03-18 11:28:40 +0100

    wfmng execution api , integration

--- masterinterface/scs_workspace/apiurls.py
+++ masterinterface/scs_workspace/apiurls.py
@@ -1,15 +1,20 @@
 __author__ = 'Alfredo Saglimbeni'
 from django.conf.urls import patterns, url
 from piston.resource import Resource
-from masterinterface.scs_workspace.api import workflows_api
+from masterinterface.scs_workspace.api import workflows_api, WfMngApiHandler
 
 
 workflows_api_entries = Resource(handler=workflows_api)
+wfmng_api = Resource(handler=WfMngApiHandler)
 
 urlpatterns = patterns(
     url(r'^workflows/(?P<global_id>[^/]+)/', workflows_api_entries),
     url(r'^workflows/(?P<global_id>[^/]+)', workflows_api_entries),
     url(r'^workflows', workflows_api_entries),
     url(r'^workflows/', workflows_api_entries),
+    url(r'^wfmng/submit/$', wfmng_api),
+    url(r'^wfmng/submit$', wfmng_api),
+    url(r'^wfmng/(?P<wfrun_id>[^/]+)/$', wfmng_api),
+    url(r'^wfmng/(?P<wfrun_id>[^/]+)$', wfmng_api),
     #url(r'^workflows(\.(?P<emitter_format>.+))$', workflows_api_entries),
 )
