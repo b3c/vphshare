4e7342f782d313c9146acaa6891c5db4dc92edd0
Alfredo Saglimbeni <a.saglimbeni@scsitaly.com>
2015-01-11 22:50:06 +0100

    send xmlquery to dataset service, render results in the UI, save/get/edit/delete query

--- masterinterface/datasets/static/js/dataset.js
+++ masterinterface/datasets/static/js/dataset.js
@@ -12,18 +12,78 @@
         var self = this;
         this.$columnsList = {};
         this.$dropArea = {"select":[], "where":[]};
-        this.$query = [];
         this.$root = root;
+        this.$globalID = root.data('globalid');
+        this.$queryid = root.data('queryid');
         $('#query_button').click(function(){
+           $('#query_button').button('loading');
            self.submit_query();
         });
+        $('#saveas-query').click(function(){
+            self.saveAsQuery();
+        });
+        $('#save-query').click(function(){
+            self.saveQuery();
+        });
+
+        $(".query-list").each(function(type,value){
+            var list = $(value);
+            list.bind('close', function () {
+                var local = $(this);
+                var id = list.data('queryid');
+                var name = list.data('name');
+                if (confirm('Are you sure you want to delete '+name+' query?')) {
+                    $.ajax({
+                        type:"POST",
+                        url:"/delete_the_query/",
+                        dataType: "json",
+                        data: {"globalID":self.$globalID, "q":id, "name":name },
+                        success: function(data){
+                            if (data['deleted'])
+                             return true;
+                        },
+                        fail: function(data){
+                            alert("Some errors occurs, retry");
+                        }
+                    });
+                    return true;
+                } else {
+                  return false;
+                }
+            })
+        });
+
         root.find("input,select,textarea").not("[type=submit]").jqBootstrapValidation();
+        root.find(".input-date").each(function(type,value) {
+            $(value).datetimepicker({clearBtn: false,
+                autoclose: true,
+                format: 'dd/mm/yyyy hh:ii'})
+        });
+
+        root.find(".input-datetime").each(function(type,value) {
+            $(value).datetimepicker({minView: 2,
+            autoclose: true,
+            clearBtn: false,
+            startView: 2,
+            format: 'dd/mm/yyyy'})
+        });
+
+        root.find(".input-time").each(function(type,value) {
+            $(value).datetimepicker({minView: 0,
+                format: 'hh:ii',
+                clearBtn: false,
+                maxView: 1,
+                autoclose: true,
+                startView: 1});
+            }
+        );
 
         root.find('.column').each(function(type,value) {
             var column = $(value);
             self.$columnsList[column.attr('id')] = {
                 name: column.data('name'),
-                type: column.data('type')
+                type: column.data('type'),
+                tablename: column.data('tablename')
             };
             column.draggable({
                 revert: "invalid",
@@ -68,6 +128,21 @@
         root.find('.connector').click(
             self.changeConnector
         );
+        self.loadtables();
+        if (this.$queryid != ""){
+            $('#query_button').button('loading');
+            self.submit_query();
+        }
+        root.find('select').change(function(){
+            var value=$( this ).val();
+            if ($.inArray(value, ['=--EMPTY', 'NOT=--EMPTY'])>-1){
+                $(this).next().hide();
+                $(this).next().val('1');
+            }else{
+                if ($(this).next()=='none') $(this).next().val('');
+                $(this).next().show();
+            }
+        });
     };
 
     QueryBuilder.prototype.changeConnector = function (){
@@ -93,20 +168,26 @@
         var name = columnObj.data('name');
         var type = columnObj.data('type');
         var id = columnObj.attr('id');
+        var tablename = columnObj.data('tablename');
         var operations = "";
         var input = "";
+        var valuetype = "";
         if ($.inArray(type, ['smallint', 'int', 'bigint']) > -1){
             operations = ich.numeric_options()[0].outerHTML;
+            valuetype = "numeric";
             input = ich.integer_value({'name':name})[0].outerHTML
         }else if ($.inArray(type, ['double', 'float']) > -1){
             operations = ich.numeric_options()[0].outerHTML;
+            valuetype = "numeric";
             input = ich.float_value({'name':name})[0].outerHTML
         }else if ($.inArray(type, ['date', 'time', 'dateTime']) > -1){
+            valuetype = type.toLowerCase();
             operations = ich.numeric_options()[0].outerHTML;
             input = ich.datetime_value({'name':name})[0].outerHTML
         }else{
             operations = ich.string_options()[0].outerHTML;
-            input = ich.text_value({'name':name})[0].outerHTML
+            input = ich.text_value({'name':name})[0].outerHTML;
+            valuetype = "string";
         }
 
         var operation = ich.inside_condition({
@@ -118,7 +199,7 @@
             var value=$( this ).val();
             if ($.inArray(value, ['=--EMPTY', 'NOT=--EMPTY'])>-1){
                 $(this).next().hide();
-                $(this).next().val('none');
+                $(this).next().val('1');
             }else{
                 if ($(this).next()=='none') $(this).next().val('');
                 $(this).next().show();
@@ -148,7 +229,9 @@
         operation.data({
            "name": name,
            "type": type,
-           "id": id
+           "id": id,
+           "tablename": tablename,
+            "valuetype": valuetype
         });
 
         QueryBuilder.drop_new_operation(target,operation);
@@ -185,6 +268,7 @@
         var name = columnObj.data('name');
         var type = columnObj.data('type');
         var id = columnObj.attr('id');
+        var tablename = columnObj.data('tablename');
         if (root.parent().find('.'+id).length){
             columnObj.remove();
             $("#select-message-error").text("This column is already in the select statement!").fadeIn().delay(5000).slideUp();
@@ -195,7 +279,8 @@
         new_select.data({
            "name": name,
            "type": type,
-           "id": id
+           "id": id,
+           "tablename": tablename
         });
         root.before(new_select);
         new_select.find('.close').tooltip();
@@ -215,8 +300,9 @@
         return true;
     };
 
-    QueryBuilder.prototype.submit_query= function () {
+    QueryBuilder.prototype.generateQuery = function(){
         var self = this;
+        $('#myTab a:last').tab('show');
         self.$dropArea = {"select":[], "where":[]};
         if (self.validation()){
 
@@ -237,8 +323,10 @@
                         condition['group'].push({
                             "name": insideCondition.data("name"),
                             "type": insideCondition.data("type"),
+                            "tablename": insideCondition.data("tablename"),
                             "operator" : insideCondition.find('.operator:first').val(),
                             "value": insideCondition.find('input:first').val(),
+                            "valueType": insideCondition.data("valuetype"),
                             "connector": insideCondition.children('.connector:visible').length > 0 ? $(insideCondition.children('.connector:visible')[0]).hasClass('and') ? "and" : "or" : ""
                         });
                     });
@@ -253,17 +341,149 @@
                     selectCondition = {
                             "name": select.data("name"),
                             "type": select.data("type"),
+                            "tablename": select.data("tablename"),
                             "displayAs": select.find('input:first').val()
                     };
                     self.$dropArea['select'].push(selectCondition)
                 }
 
             });
+            return true;
+        }else{
+            return false;
         }
+    };
+
+    QueryBuilder.prototype.submit_query= function () {
+        var self = this;
+        if (self.validation()){
+            self.generateQuery();
+            //Send query to server and retriave the results.
+            $.ajax({
+                type:"POST",
+                url:"/get_results/",
+                dataType: "json",
+                data: {"globalID":self.$globalID, "query":JSON.stringify(self.$dropArea) },
+                success: function(data){
+                    self.getReults(data);
+                },
+                fail: function(data){
+                    alert("Some errors occurs, retry");
+                }
+            })
+        }
+    };
+
+    QueryBuilder.prototype.getReults = function (results) {
+        var self = this;
+        var header = [];
+        for (column in results['header']){
+            header.push({"title":results['header'][column]});
+        }
+        var table = $('<table cellpadding="0" cellspacing="0" border="0" class="display cell-border" id="dataset-results-table"></table>');
+        $("#query-results > .span12").empty();
+        table.appendTo("#query-results > .span12");
+        $("#query-results").show();
+        $("#hr-query-results").show();
+        table.dataTable({
+                /*"data": dataSet,*/
+                "data": results['results'],
+                "columns": header,
+                "paging": true,
+                "ordering": true,
+                "info": true,
+                "search": true,
+                "scrollX": true,
+                language: {
+                    search: "<i class='icon-search'></i>",
+                    paginate: {
+                        previous: "«",
+                        next: "»"
+                    }
+                }
+        });
+        $('#query_button').button('reset');
+    };
+    QueryBuilder.prototype.saveQuery = function () {
+        var self = this;
+        $("#save-query").button('loading');
+        if (!self.generateQuery()) {
+            $("#save-query").button('reset');
+            $('#save-query-modal').modal('toggle');
+            return false;
+        }
+        if ( $("#save-query-input").val() == "") {
+            $("#save-query").button('reset');
+            return false;
+        }
+        var name = $("#save-query-input").val();
+        $.ajax({
+                type:"POST",
+                url:"/edit_the_query/",
+                dataType: "json",
+                data: {"globalID":self.$globalID, "query":JSON.stringify(self.$dropArea), "name":name , "q":self.$queryid},
+                success: function(data){
+                    if (data['saved'])
+                    window.location.replace("/query_builder/"+self.$globalID+"/?q="+data['id']);
+                },
+                fail: function(data){
+                    alert("Some errors occurs, retry");
+                }
+            });
+    };
+
+    QueryBuilder.prototype.saveAsQuery = function () {
+        var self = this;
+        $("#saveas-query").button('loading');
+        if (!self.generateQuery()) {
+            $("#saveas-query").button('reset');
+            $('#saveas-query-modal').modal('toggle');
+            return false;
+        }
+        if ( $("#saveas-query-input").val() == "") {
+            $("#saveas-query").button('reset');
+            return false;
+        }
+        var name = $("#saveas-query-input").val();
+        $.ajax({
+                type:"POST",
+                url:"/save_the_query/",
+                dataType: "json",
+                data: {"globalID":self.$globalID, "query":JSON.stringify(self.$dropArea), "name":name },
+                success: function(data){
+                    if (data['saved'])
+                    window.location.replace("/query_builder/"+self.$globalID+"/?q="+data['id']);
+                },
+                fail: function(data){
+                    alert("Some errors occurs, retry");
+                }
+            });
     };
 
     QueryBuilder.prototype.loadtables = function () {
         var self = this;
+        self.$root.find('.dataset-table').each(function () {
+            $(this).dataTable({
+
+                "dom": "<'row-fluid'<'span12'f>r>" + "<'row-fluid'<'span12'p>>" + "<'row-fluid'<'span12't>>",
+                /*"data": dataSet,*/
+                "columns": [
+                    { "title": "" }
+                ],
+                "paging": true,
+                "ordering": false,
+                "info": false,
+                "search": true,
+                "scrollX": true,
+                language: {
+                    search: "<i class='icon-search'></i>",
+                    paginate: {
+                        previous: "«",
+                        next: "»"
+                    }
+                }
+            });
+          });
     };
 
     QueryBuilder.autoDiscover = function() {
--- masterinterface/datasets/urls.py
+++ masterinterface/datasets/urls.py
@@ -8,5 +8,9 @@ urlpatterns = patterns(
     'scs.views',
     url(r'^query_builder/(?P<global_id>[\w\-]+)/$', query_builder, name='query_builder'),
     url(r'^get_dataset_schema/$', get_dataset_schema, name='get_dataset_schema'),
+    url(r'^get_results/$', get_results, name='get_results'),
+    url(r'^save_the_query/$', save_the_query, name='save_the_query'),
+    url(r'^edit_the_query/$', edit_the_query, name='edit_the_query'),
+    url(r'^delete_the_query/$', delete_the_query, name='edit_the_query'),
 )
 
