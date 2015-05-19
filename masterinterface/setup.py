a80d171d6257f4eb5db4a52abb226471290c19c4
Milton <m.hoz@sheffield.ac.uk>
2015-05-19 19:59:50 +0000

    update setup with the new requirements

--- masterinterface/setup.py
+++ masterinterface/setup.py
@@ -16,7 +16,7 @@ METADATA = dict(
     long_description=open('../README.rst').read(),
     url='http://github.com/b3c/vphshare',
     keywords='django openid registration github vphshare',
-    install_requires=['django<=1.4.5', 'oauth2==1.5.167', 'python_openid<=2.2.5', 'django-social-auth<=0.6.9', 'suds<=0.4', 'pysimplesoap<=1.05a', 'django-piston<=0.2.3','M2Crypto<=0.21.1','pycrypto<=2.6','mod-auth-library', 'requests<=1.2.0', 'lxml<=2.3.6', 'ordereddict<=1.1','django-workflows<=1.0.2','django-permissions<=1.0.3', 'xmltodict<=0.8.6','django-datetime-widget<=0.9.2', 'django_select2<=4.2.2', 'celery<=3.1.9', 'django-celery<=3.1.1', 'raven', 'django-paintstore'],
+    install_requires=['django<=1.4.5', 'oauth2==1.5.167', 'python_openid<=2.2.5', 'django-social-auth<=0.6.9', 'suds<=0.4', 'pysimplesoap<=1.05a', 'django-piston<=0.2.3','M2Crypto<=0.21.1','pycrypto<=2.6','mod-auth-library', 'requests<=1.2.0', 'lxml<=2.3.6', 'ordereddict<=1.1','django-workflows<=1.0.2','django-permissions<=1.0.3', 'xmltodict<=0.8.6','django-datetime-widget<=0.9.2', 'django_select2<=4.2.2', 'celery<=3.1.9', 'django-celery<=3.1.1', 'raven', 'django-paintstore', 'jsonpath-rw'],
     include_package_data=True,
     classifiers=[
         'Development Status :: 4 - Beta',
