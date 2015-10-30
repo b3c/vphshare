8768df6935f5712a016a532239daf935ee86d3bc
Miguel Carmona <mkarmona@users.noreply.github.com>
2015-10-30 13:27:31 +0000

    wip(feature-jqapi): drafting api requests
    References DARE-262

--- masterinterface/cilab_ejobs/api.py
+++ masterinterface/cilab_ejobs/api.py
@@ -3,6 +3,8 @@ import base64
 from django.db.models import Q
 from permissions.models import PrincipalRoleRelation
 from django.http import HttpResponse
+from django.core import serializers
+from django.core.exceptions import ObjectDoesNotExist
 from piston.handler import BaseHandler
 from masterinterface.scs_auth.auth import authenticate, getUserTokens
 from masterinterface.scs_auth import models
@@ -28,25 +30,35 @@ class EJobsAPIHandler(BaseHandler):
     allowed_methods = ('POST', 'GET', 'PUT', 'DELETE')
 
     def create(self, request, *args, **kwargs):
+        """the post method to post a job into the queue.
+
+            no uri args but you have to post with params
+            worker_id external worker id
+            data the input data (json dict) to include into the job
+        """
         ticket = _check_header_ticket(request)
 
         if ticket is not None:
             uname = ticket[1]
-            uid = _get_id_and_check_tokens(uname,"producer")
+            uid = _get_id_and_check_tokens(uname,set(["producer"]))
 
             if uid:
                 try:
-                    # TODO now this user can submit a job
-                    M.ejob_submit(uid,0,"")
+                    worker_id = int( request.POST.get("worker_id","-1") )
+                    input_data = request.POST.get("data","")
+                    logger.debug( "post with args %d %s" % (worker_id,input_data) )
+                    M.ejob_submit(uid,worker_id,input_data)
                     return { "username": uname }
 
                 except M.EJobException, e:
                     logger.exception(e)
-                    return rc.FORBIDDEN
+                    return rc.BAD_REQUEST
 
             else:
+                logger.error("ejob failed to get id and check tokens")
                 return rc.FORBIDDEN
         else:
+            logger.error("ejob failed to check ticket")
             return rc.FORBIDDEN
 
 
@@ -55,17 +67,29 @@ class EJobsAPIHandler(BaseHandler):
 
         if ticket is not None:
             uname = ticket[1]
-            uid = _get_id_and_check_tokens(uname,"producer")
+            uid = _get_id_and_check_tokens(uname,set(["producer","consumer"]))
 
             if uid:
                 try:
-                    # TODO if user can get tasks
-                    # get tasks from db
-                    return { "username": uname }
+                    #get from only one task with id global_id
+                    if global_id:
+                        try:
+                            l = M.EJob.objects.get(Q(id__exact=global_id),
+                                                Q(owner_id__exact=uid) | Q(worker_id__exact=uid) )
+
+                            #r = serializers.serialize("json",l)
+                            return l
+                        except ObjectDoesNotExist, e:
+                            logger.exception(e)
+                            return rc.NOT_FOUND
+
+                    #get a list of tasks
+                    else:
+                        return { "username": uname }
 
                 except M.EJobException, e:
                     logger.exception(e)
-                    return rc.FORBIDDEN
+                    return rc.BAD_REQUEST
             else:
                 return rc.FORBIDDEN
         else:
@@ -76,7 +100,7 @@ class EJobsAPIHandler(BaseHandler):
 
         if ticket is not None:
             uname = ticket[1]
-            uid = _get_id_and_check_tokens(uname,"consumer")
+            uid = _get_id_and_check_tokens(uname,set(["consumer"]))
 
             if uid:
                 try:
@@ -85,7 +109,7 @@ class EJobsAPIHandler(BaseHandler):
                     return { "username": uname }
                 except M.EJobException, e:
                     logger.exception(e)
-                    return rc.FORBIDDEN
+                    return rc.BAD_REQUEST
             else:
                 return rc.FORBIDDEN
         else:
@@ -97,12 +121,16 @@ class EJobsAPIHandler(BaseHandler):
 
         if ticket is not None:
             uname = ticket[1]
-            uid = _get_id_and_check_tokens(uname,"producer")
+            uid = _get_id_and_check_tokens(uname,set(["producer"]))
 
             if uid:
-                # TODO now this user can submit a job
-                M.ejob_cancel(0,uid)
-                return { "username": uname }
+                try:
+                    # TODO now this user can submit a job
+                    M.ejob_cancel(0,uid)
+                    return { "username": uname }
+                except M.EJobException, e:
+                    logger.exception(e)
+                    return rc.BAD_REQUEST
             else:
                 return rc.FORBIDDEN
         else:
@@ -134,15 +162,15 @@ def _check_header_ticket(req):
         logger.debug( "checked ticket for %s" %(ticket[1],) )
         return ticket
 
-def _get_id_and_check_tokens(uname,token_name):
-    """get _id if token_name in user tokens else None
+def _get_id_and_check_tokens(uname,token_set):
+    """get _id if token_set in user tokens else None
     """
     user = models.User.objects.get(username=uname)
     if user is not None:
         tokens = getUserTokens(user)
         logger.debug( "getid and check tokens %s" % (str(tokens),) )
 
-        if (token_name in tokens):
+        if (len(tokens.intersection(token_set)) > 0):
             logger.debug("user with correct token")
             return user.id
         else:
