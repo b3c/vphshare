27f788079a060bbabfa184b10f4872f0396a47ec
Miguel Carmona <mkarmona@users.noreply.github.com>
2015-11-02 10:47:15 +0000

    fix: naive datetime used with django
    Fixes DARE-262

--- masterinterface/cilab_ejobs/management/commands/ejobsclean.py
+++ masterinterface/cilab_ejobs/management/commands/ejobsclean.py
@@ -1,7 +1,8 @@
 from django.db.models import Q
 from django.core.management.base import BaseCommand, CommandError
+from django.utils import timezone
 from cilab_ejobs.models import EJob
-from datetime import date, timedelta
+from datetime import timedelta
 
 class Command(BaseCommand):
     args = '<past_days past_days ...>'
@@ -11,8 +12,8 @@ class Command(BaseCommand):
         for pass_days in args:
             try:
                 days = int(pass_days)
-                date_start = date(2015,1,1)
-                date_end = date.today() - timedelta(days=days)
+                date_start = timezone.datetime.date(2015,1,1)
+                date_end = timezone.now - timedelta(days=days)
                 ejobs = EJob.objects.filter(
                         Q(state__gt=1),
                         Q(modification_timestamp__range=(date_start,date_end)) )
