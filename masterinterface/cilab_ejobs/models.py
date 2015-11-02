15ee87653d7269da807b7dd3dad98d4044d51ae5
Miguel Carmona <mkarmona@users.noreply.github.com>
2015-11-02 13:01:00 +0000

    fix: nstates set from no iterables & correct next_state
    Fixes DARE-262

--- masterinterface/cilab_ejobs/models.py
+++ masterinterface/cilab_ejobs/models.py
@@ -61,8 +61,8 @@ def ejob_transit(job_id, worker_id, data):
     # ckeck next state exists and transit
     # return transited ejob if success else EJobException is raised
     # raise EJobException("error message")
-    submited_nstates = set(EJob.ST_STARTED,)
-    started_nstates = set(EJob.ST_COMPLETED, EJob.ST_FAILED)
+    submited_nstates = set([EJob.ST_STARTED])
+    started_nstates = set([EJob.ST_COMPLETED, EJob.ST_FAILED])
 
     ej = EJob.objects.get(Q(id__exact=job_id),Q(worker_id__exact=worker_id))
 
@@ -71,12 +71,12 @@ def ejob_transit(job_id, worker_id, data):
     st = ej.state
     # start or cancel
     if st == EJob.ST_SUBMITED and next_state in submited_nstates:
-        ej.state = st
+        ej.state = next_state
         ej.save()
 
     # finish or cancel
     elif st == EJob.ST_STARTED and next_state in started_nstates:
-        ej.state = st
+        ej.state = next_state
         ej.output_data = output_data
         ej.save()
 
